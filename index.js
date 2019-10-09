d0aab20181675b8380cc8ebe6d58d2ec631bb860
dhananzen <dhananjayk.456@gmail.com>
2019-10-09 23:52:55 +0530

    Adds checkbox next to item to mark item as done/undone

--- index.js
+++ index.js
@@ -6,24 +6,11 @@
     var todoList = document.getElementById(TODO_LIST_SELECTOR);
 
     let todos = [];
-    // let idCounter = 0;
+    let idCounter = 0;
 
     const clearInput = function () {
         todoInput.value = "";
         return;
-    }
-
-    const createTodoItem = function (todoString) {
-        var todoItem = document.createElement("div");
-        var isDoneCheckBox = document.createElement("input");
-
-        // isDoneCheckBox.type = "checkbox";
-        // isDoneCheckBox.value = ""
-        // todoItem.appendChild()
-
-        todoItem.textContent = todoString;
-        todoItem.className = "item";
-        return todoItem;
     }
 
     const removeAllExistingTodos = function () {
@@ -43,7 +30,11 @@
             isEnterKeyPressed = keyEntered === 13;
 
         if (isEnterKeyPressed) {
-            todos.push(e.target.value);
+
+            const todoValue = e.target.value,
+                todoId = idCounter++;
+
+            todos.push({ label: todoValue, id: todoId });
             clearInput();
             renderTodos();
         }
@@ -53,3 +44,38 @@
     todoInput.addEventListener('keydown', handleKeyDownInInput);
 
 })();
+
+
+
+const createTodoItem = function (todoObject) {
+    var todoItem = document.createElement("div");
+    todoItem.className = "item";
+
+    var isDone = renderCheckBox(todoObject, todoItem);
+    todoItem.appendChild(isDone);
+
+    var todoLabel = renderTodoLabel(todoObject);
+    todoItem.appendChild(todoLabel);
+
+    return todoItem;
+}
+
+function renderTodoLabel(todoObject) {
+    var todoLabel = document.createElement("span");
+    var textNode = document.createTextNode(todoObject.label);
+    todoLabel.appendChild(textNode);
+
+    return todoLabel;
+}
+
+function renderCheckBox(todoObject) {
+    var isDoneCheckBox = document.createElement("input");
+    isDoneCheckBox.type = "checkbox";
+    isDoneCheckBox.value = todoObject.id;
+    isDoneCheckBox.checked = false;
+    isDoneCheckBox.onclick = function (e) {
+        console.log("onClick", e.target.value);
+    }
+
+    return isDoneCheckBox;
+}
